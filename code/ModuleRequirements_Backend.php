3b9c3c0078ef5dd7957fc01e069587c059915360
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:53:46 +1300

    Converted to PSR-2

--- code/ModuleRequirements_Backend.php
+++ code/ModuleRequirements_Backend.php
@@ -1,15 +1,17 @@
 <?php
 
-class ModuleRequirements_Backend extends Requirements_Backend {
-	
-	protected function path_for_file($fileOrUrl) {
-		if(preg_match('{^//|http[s]?}', $fileOrUrl)) {
-			return $fileOrUrl;	
-		} else {
-			if(!Director::fileExists($fileOrUrl)) {
-				$fileOrUrl = MODULES_DIR . '/' . $fileOrUrl;
-			}
-			return parent::path_for_file($fileOrUrl);
-		}
-	}
+class ModuleRequirements_Backend extends Requirements_Backend
+{
+    
+    protected function path_for_file($fileOrUrl)
+    {
+        if (preg_match('{^//|http[s]?}', $fileOrUrl)) {
+            return $fileOrUrl;
+        } else {
+            if (!Director::fileExists($fileOrUrl)) {
+                $fileOrUrl = MODULES_DIR . '/' . $fileOrUrl;
+            }
+            return parent::path_for_file($fileOrUrl);
+        }
+    }
 }
